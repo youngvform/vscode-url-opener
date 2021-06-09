dbff40c98863341c3891f6f605d45eae2b7daeb3
devdevdeep <vformu@naver.com>
2021-06-10 00:20:24 +0900

    feat: add search keyword feature

--- src/extension.ts
+++ src/extension.ts
@@ -14,54 +14,107 @@ export function activate(context: vscode.ExtensionContext) {
   // Now provide the implementation of the command with registerCommand
   // The commandId parameter must match the command field in package.json
   let urlDisposable = vscode.commands.registerCommand(
-    'url-opener.openAnUrl',
-    openAnUrl
+    'url-opener.openUrl',
+    openUrl
   );
   let selectedDisposable = vscode.commands.registerCommand(
-    'url-opener.openAnSelectedUrl',
-    openAnSelectedUrl
+    'url-opener.openSelectedUrl',
+    openSelectedUrl
+  );
+  let searchDisposable = vscode.commands.registerCommand(
+    'url-opener.searchKeyword',
+    searchKeyword
+  );
+  let searchSelectedDisposable = vscode.commands.registerCommand(
+    'url-opener.searchSelectedKeyword',
+    searchSelectedKeyword
   );
 
   context.subscriptions.push(urlDisposable);
   context.subscriptions.push(selectedDisposable);
+  context.subscriptions.push(searchDisposable);
+  context.subscriptions.push(searchSelectedDisposable);
 }
 
-function openAnUrl() {
-  const placeHolder = 'Type an url';
+function showInputBox(
+  callback: (urlOrKeyword: string) => void,
+  placeHolder: string
+) {
   const input = vscode.window.showInputBox({ placeHolder });
-  input.then((url) => {
-    if (url === undefined) {
+  input.then((userInput) => {
+    if (userInput === undefined) {
       return;
     }
 
-    if (url === '') {
-      vscode.window.showInformationMessage('You need to type an url!');
+    if (userInput === '') {
+      vscode.window.showInformationMessage('Please type a valid one!');
       return;
     }
 
-    openDefaultBrowser(url);
+    callback(userInput);
   });
 }
 
-function openAnSelectedUrl() {
-  console.log('your selected url is ');
+function getSelectedText() {
   const activeEditor = vscode.window.activeTextEditor;
   if (!activeEditor) {
-    vscode.window.showInformationMessage('You need to have a file opened!');
+    vscode.window.showInformationMessage('Please have a file opened!');
     return;
   }
   const selection = activeEditor.selection;
-  const text = activeEditor.document.getText(selection);
+  return activeEditor.document.getText(selection);
+}
 
-  openDefaultBrowser(text);
+function isValidHttpUrl(url: string) {
+  const pattern = new RegExp(
+    '^(https?:\\/\\/)?' +
+      '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' +
+      '((\\d{1,3}\\.){3}\\d{1,3}))' +
+      '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' +
+      '(\\?[;&a-z\\d%_.~+=-]*)?' +
+      '(\\#[-a-z\\d_]*)?$',
+    'i'
+  );
+  return !!pattern.test(url);
 }
 
 async function openDefaultBrowser(url: string) {
-  let _url = url;
-  if (!_url.includes('http')) {
-    _url = `http://${url}`;
+  const _url = url.includes('http') ? url : `http://${url}`;
+
+  if (!isValidHttpUrl(_url)) {
+    vscode.window.showInformationMessage('Please check a url!');
+    return;
   }
+
   await open(_url);
+}
+
+const search = (keyword: string) => {
+  const configSearchUrl =
+    vscode.workspace.getConfiguration('url-opener').defaultSearchUrl;
+  if (!configSearchUrl) return;
+  const url = configSearchUrl + keyword.replace(/\s/g, '+');
+  openDefaultBrowser(url);
+};
+
+function openUrl() {
+  showInputBox(openDefaultBrowser, 'Type a url');
+}
+
+function openSelectedUrl() {
+  const text = getSelectedText();
+  if (!text) return;
+  openDefaultBrowser(text);
+}
+
+function searchKeyword() {
+  showInputBox(search, 'Type keywords for search');
+}
+
+function searchSelectedKeyword() {
+  const text = getSelectedText();
+  if (!text) return;
+  search(text);
 }
 
 // this method is called when your extension is deactivated
